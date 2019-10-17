59980774028fa6d175543e08fe0d0cfefe60a344
Sergei Maertens <sergei@maykinmedia.nl>
2019-10-17 11:58:13 +0200

    :white_check_mark: Add tests for exact lookups

--- tests/test_querying.py
+++ tests/test_querying.py
@@ -98,3 +98,39 @@ def test_in_lookup_mixed_local_remote():
     ).order_by("pk")
 
     assert list(qs) == [zaak1, zaak2]
+
+
+def test_exact_lookup_local_fk():
+    local_zaaktype = ZaakType.objects.create(name="local")
+    zaak1 = Zaak.objects.create(zaaktype=local_zaaktype)
+    Zaak.objects.create(zaaktype="https://example.com/zt/123")
+
+    qs = Zaak.objects.filter(zaaktype=local_zaaktype)
+
+    assert list(qs) == [zaak1]
+
+
+def test_exact_lookup_with_url_value():
+    local_zaaktype = ZaakType.objects.create(name="local")
+    Zaak.objects.create(zaaktype=local_zaaktype)
+    zaak2 = Zaak.objects.create(zaaktype="https://example.com/zt/123")
+
+    qs = Zaak.objects.filter(zaaktype="https://example.com/zt/123")
+
+    assert list(qs) == [zaak2]
+
+
+def test_exact_lookup_with_virtual_model():
+    local_zaaktype = ZaakType.objects.create(name="local")
+    Zaak.objects.create(zaaktype=local_zaaktype)
+    zaak2 = Zaak.objects.create(zaaktype="https://example.com/zt/123")
+    with requests_mock.Mocker() as m:
+        m.get(
+            "https://example.com/zt/123",
+            json={"url": "https://example.com/zt/123", "name": "remote"},
+        )
+        zaaktype = zaak2.zaaktype
+
+    qs = Zaak.objects.filter(zaaktype=zaaktype)
+
+    assert list(qs) == [zaak2]
