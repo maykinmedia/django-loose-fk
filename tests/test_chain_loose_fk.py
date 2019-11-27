28ee37db0a67f51b782c5a7196d120b8f88c955a
annashamray <anna.shamray@maykinmedia.nl>
2019-11-27 12:52:18 +0100

    add regression test for requesting local loose-fk in external loose-fk

---
+++ tests/test_chain_loose_fk.py
@@ -0,0 +1,28 @@
+"""
+Test the API interface to handle chained local/remote references.
+"""
+import pytest
+import requests_mock
+from rest_framework.reverse import reverse
+from testapp.models import ZaakType, ZaakObject
+
+pytestmark = pytest.mark.django_db()
+
+
+def test_get_local_fk_after_remote_url(api_client):
+    zaaktype = ZaakType.objects.create(name="test")
+    zaaktype_url = reverse("zaaktype-detail", kwargs={"pk": zaaktype.pk})
+    zaak_url = "https://example.com/zaaktypen/123"
+
+    url = reverse("zaakobject-list")
+
+    with requests_mock.Mocker() as m:
+        m.get(zaak_url, json={"url": zaak_url, "name": "test", "zaaktype": f'http://testserver.com{zaaktype_url}'})
+
+        response = api_client.post(url, {"name": "test", "zaak": zaak_url}, HTTP_HOST="testserver.com")
+
+        assert response.status_code == 201
+        zaakobject = ZaakObject.objects.get()
+
+        with pytest.raises(requests_mock.NoMockAddress):
+            zaakobject.zaak.zaaktype
